4490d26e08125729607a70090d05dad65da41762
Hyung Kim <hjkmines@gmail.com>
2020-09-01 14:21:45 -0600

    adds boiler plate

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import React, { Component } from 'react';
-import Container from './components/Containers/Container'
+import React from 'react'
 
-class App extends Component {
-
-state = {
-  characters: []
-}
-
-componentDidMount() {
-  fetch('https://rickandmortyapi.com/api/character/')
-    .then(response => response.json())                                               
-    .then(result => this.setState({
-      characters: result.results 
-    }))
-}
-
-render() {
+function App() {
   return (
-    <div className='App'> 
-    {this.state.characters.length > 0 ? <Container characters={this.state.characters}/> : null}
-    </div>
+  <div className='App'>
+
+  </div>
   )
-}
 }
 
 export default App; 
--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const App = () => {
+    <div>
+
+    </div>
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
