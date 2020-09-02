849ee80401bb0587512ddd0c86eba5567c1b5860
Hyung Kim <hjkmines@gmail.com>
2020-09-01 21:24:43 -0600

    adds two more profile components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker'; 
 // import App from './App';
 
 const App = () => {
@@ -7,7 +8,35 @@ const App = () => {
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" />
+                    <img alt="avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam 
+                    </a>
+                    <div>
+                        <span className="date">Today at 6:00 PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam 
+                    </a>
+                    <div>
+                        <span className="date">Today at 6:00 PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()} />
                 </a>
                 <div className="content">
                     <a href="/" className="author">
