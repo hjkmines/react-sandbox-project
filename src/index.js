7e4903f319ba75def672d37fce7fe724e4a0a44b
Hyung Kim <hjkmines@gmail.com>
2020-09-02 20:49:03 -0600

    adds reusable approval card component

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ import faker from 'faker';
 const App = () => {
     return (
         <div className="ui container comments">
+
+           <ApprovalCard>
+               <div>
+               <h4>Warning!</h4>
+               Are you sure you want to do this?
+               </div> 
+           </ApprovalCard>
+
            <ApprovalCard>
            <CommentDetail 
            author="Sam"
@@ -15,18 +23,24 @@ const App = () => {
            image={faker.image.avatar()}
            /> 
            </ApprovalCard>
+
+           <ApprovalCard> 
            <CommentDetail 
            author="Alex" 
            timeAgo="Today at 1:00 PM" 
            comment="Nice post" 
            image={faker.image.avatar()}
            /> 
+           </ApprovalCard> 
+           
+           <ApprovalCard>
            <CommentDetail 
            author="Jane" 
            timeAgo="Today at 12:00 PM" 
            comment="What are you up to?" 
            image={faker.image.avatar()}
            /> 
+           </ApprovalCard>
         </div>
     )
 }
