97d1e44cad8933ae7b67a471bf04ceab6cfa0a0b
Hyung Kim <hjkmines@gmail.com>
2020-09-03 14:25:01 -0600

    adds reusuable component to SeasonDisplay comp

--- src/index.js
+++ src/index.js
@@ -1,32 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react'; 
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';  
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props); 
+    state = { lat: null, errorMessage: '' }
 
-        this.state = { lat: null, errorMessage: '' }; 
-        
-    }
-    
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude })
-            }, 
-            (err) => {
-                this.setState({ errorMessage: err.message })
-            } 
+            position => this.setState({ lat: position.coords.latitude }), 
+            err => {this.setState({ errorMessage: err.message })} 
         )
-    }
-
-    componentDidUpdate() {
-        console.log('Runs many times when needed, good place to do more data loading when state/props change')
-    }
-
-    componentWillUnmount() {
-        console.log('good place to clean up')
     }
 
     render() {
@@ -34,7 +18,7 @@ class App extends React.Component {
             return <div>Error: {this.state.errorMessage}</div>
         }
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
         return <div>Loading!</div> 
     }
