321c8118a5cc4d65b956fccd8d95f6de473364e1
Hyung Kim <hjkmines@gmail.com>
2020-09-07 11:44:56 -0600

    set up network request for fetching youtube videos

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React from 'react'; 
 import SearchBar from './SearchBar'; 
+import youtube from '../apis/youtube'; 
+import VideoList from './VideoList'; 
+import VideoDetail from './VideoDetail'; 
+
+const KEY= 'sdfiuhjduducj84894sdfasdf'; 
 
 class App extends React.Component {
     render() {
