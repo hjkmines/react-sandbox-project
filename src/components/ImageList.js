9d5eaf63d675ddece01b3d99d1842d6b38c13c4f
Hyung Kim <hjkmines@gmail.com>
2020-09-06 19:15:45 -0600

    adds imagelist css

---
+++ src/components/ImageList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ImageList' 
+
+const ImageList = (props) => {
+    const images = props.images.map( ({ description, id, urls }) => {
+        return <img 
+        key={id}
+        src={urls.regular}
+        alt={description}
+        />
+    })
+
+    return <div className='image-list' >{images}</div>
+}
+
+export default ImageList; 
