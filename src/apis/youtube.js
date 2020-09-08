8bef20be12903bb0b86e676c397c74a2d0231fbe
Hyung Kim <hjkmines@gmail.com>
2020-09-07 20:52:29 -0600

    adds video item component

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,7 +1,14 @@
 import axios from 'axios'; 
+const KEY ='adfoijdi5325fdsaf5sdf5d5f4sd8484'
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3'
+    baseURL: 'https://www.googleapis.com/youtube/v3', 
+    params: {
+        part: 'snippet', 
+        type: 'video', 
+        maxResults: 5, 
+        key: KEY 
+    }
 }); 
 
 
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,14 @@
 import React from 'react'; 
+import VideoItem from './VideoItem';
 
-class VideoList extends React.Component {
-    render() {
-        return (
-            <div>VideoList</div>
-        )
-    }
+const VideoList = ({ videos }) => {
+    const renderedList = videos.map( video => {
+        return <VideoItem video={video} />; 
+    })
+
+    return (
+        <div>{renderedList}</div>
+    )
 }
 
 export default VideoList; 
