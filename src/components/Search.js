dac011a811ae4d86f4b2cd8be0e9545d88f5ddea
Hyung Kim <hjkmines@gmail.com>
2020-09-13 18:22:34 -0600

    adds dropdown component

--- src/components/Search.js
+++ src/components/Search.js
@@ -20,7 +20,22 @@ const Search = () => {
             setResults(data.query.search)
         }
 
-        search(); 
+        if (term && !results.length) {
+            search(); 
+        } else {
+            
+        }
+        
+        const timeoutId = setTimeout( () => {
+            if(term) {
+                search() 
+            }
+        }, 1000)
+
+        return () => {
+            clearTimeout(timeoutId)
+        }
+
     }, [term])
 
     const renderedResults = results.map( result => {
