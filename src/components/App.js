4e15a88189496ab389ba1e0e25de30524d0a7573
Hyung Kim <hjkmines@gmail.com>
2020-09-15 12:24:50 -0600

    adds header component to connect with react router

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import Accordion from './Accordion';
 import Search from './Search'; 
 import Dropdown from './Dropdown'; 
 import Translate from './Translate'; 
+import Route from './Route'; 
+import Header from './Header'; 
 
 const items = [
     {
@@ -34,40 +36,29 @@ const options = [
     }
 ]; 
 
-const showAccordion = () => {
-    if (window.location.pathname === '/') {
-        return <Accordion items={items} /> 
-    }
-}
-
-const showList = () => {
-    if (window.location.pathname === '/list') {
-        return <Search /> 
-    }
-}
-
-const showDropdown = () => {
-    if (window.location.pathname === '/dropdown') {
-        return <Dropdown /> 
-    }
-}
-
-const showTranslate = () => {
-    if (window.location.pathname === '/translate') {
-        return <Translate /> 
-    }
-}
-
 export default () => {
     const [selected, setSelected] = useState(options[0])
-    const [showDropdown, setShowDropdown] = useState(true)
 
     return (
         <div>
-        {showAccordion()}
-        {showList()}
-        {showDropdown()}
-        {showTranslate()}
+            <Header /> 
+            <Route path='/'>
+                <Accordion items={items} /> 
+            </Route>
+            <Route path='/list'> 
+                <Search /> 
+            </Route>
+            <Route path='/dropdown'> 
+                <Dropdown 
+                    label='Select a color'
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                /> 
+            </Route>
+            <Route path='/translate'> 
+                <Translate /> 
+            </Route>
         </div>
     )
 }; 
