1cff5a21cb911e19ffb96c847fec1180df3f211f
Hyung Kim <hjkmines@gmail.com>
2020-09-15 20:20:29 -0600

    adds second solution to reg ex problem

--- src/Sandbox.js
+++ src/Sandbox.js
@@ -21,3 +21,25 @@ console.log("You have entered an invalid email address!");
 }
 
 valid_email('me-info@example.com');
+
+
+// Write a JavaScript program to count number of words in string.
+
+// The script will be used to:
+
+// - Remove white-space from start and end position.
+// - Convert 2 or more spaces to 1.
+// - Exclude newline with a start spacing.
+// Pictorial Presentation:
+
+function count_words()
+{
+str1= document.getElementById("InputText").value;
+//exclude  start and end white-space
+str1 = str1.replace(/(^\s*)|(\s*$)/gi,"");
+//convert 2 or more spaces to 1  
+str1 = str1.replace(/[ ]{2,}/gi," ");
+// exclude newline with a start spacing  
+str1 = str1.replace(/\n /,"\n");
+document.getElementById("noofwords").value = str1.split(' ').length;
+}
