290eec6f8b830a3b0727249f457a0e35c048aaee
Hyung Kim <hjkmines@gmail.com>
2020-09-15 20:22:41 -0600

    adds fourth solution to regex

--- src/Sandbox.js
+++ src/Sandbox.js
@@ -82,3 +82,47 @@ console.log(is_timeString("11:35:30"));
 
 console.log(is_timeString("90:90:90"));
 
+
+Write a JavaScript function to check whether a given value is Canada Post Code or not.
+
+function is_caPostalCode(str)
+{
+ regexp = /^(?!.*[DFIOQU])[A-VXY][0-9][A-Z]\s?[0-9][A-Z][0-9]$/;
+  
+        if (regexp.test(str))
+          {
+            return true;
+          }
+        else
+          {
+            return false;
+          }
+}
+
+console.log(is_caPostalCode("K8V3Y1"));
+
+console.log(is_caPostalCode("K8V 3Y1"));
+
+console.log(is_caPostalCode("Z4V4X1"));
+
+
+function is_html(str)
+{
+ regexp = /<([a-z]+) *[^/]*?>/;
+  
+        if (regexp.test(str))
+          {
+            return true;
+          }
+        else
+          {
+            return false;
+          }
+}
+
+console.log(is_html(''));
+
+console.log(is_html(''));
+
+console.log(is_html('.selector'));
+
